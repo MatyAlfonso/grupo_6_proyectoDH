c4e42aa021d23b4d9eb29be7f1fbe75e15b9db12
MatyAlfonso <matt1998boca@gmail.com>
2020-01-16 18:24:52 -0300

    Mejoras generales, validacion de mail y password

--- proyecto-dh/src/routes/users.js
+++ proyecto-dh/src/routes/users.js
@@ -1,13 +1,57 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { check, validationResult, body } = require('express-validator');
+const fs = require('fs');
+
+let userValidation = [
+  check('email').isEmail().withMessage('► El campo debe ser un email válido'),
+  check('password').isLength({ min: 4, max: 20 }).withMessage('► El password debe tener entre 4 y 20 caracteres'),
+]
 
 /* GET users listing. */
-router.get('/login', function(req, res, next) {
+router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
 router.get('/register', function (req, res) {
-    res.render('register')
+  res.render('register');
 });
+
+router.post('/register', userValidation, function (req, res) {
+  let result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    return res.render('register', {
+      errors: result.errors,
+      data: req.body
+    })
+  }
+
+  let users = fs.readFileSync('src/data/users.json', { encoding: 'utf-8' })
+  users = JSON.parse(users);
+  users.push(req.body);
+  users = JSON.stringify(users);
+  fs.writeFileSync('src/data/users.json', users);
+
+  res.redirect(301, '/users/login');
+});
+
+router.post('/login', userValidation, function (req, res) {
+  let result = validationResult(req);
+  console.log(result);
+  if (!result.isEmpty()) {
+    return res.render('login', {
+      errors: result.errors,
+      data: req.body
+    })
+  }
+
+  res.redirect(301, '/users/welcome');
+});
+
+router.get('/welcome',function (req, res) {
+  res.end(`Bienvenido: `);
+});
+
 
 module.exports = router;

92c340ccf4d827859c8f322478000e34e8b26b7a
MatyAlfonso <matt1998boca@gmail.com>
2020-02-03 12:39:12 -0300

    agregado el session funcionando

---
+++ proyecto-dh/src/controllers/usersController.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const { check, validationResult, body } = require('express-validator');
+const fs = require('fs');
+const bcrypt = require('bcrypt');
+
+let usersController = {
+    register: function (req, res) {
+        return res.render('register');
+    },
+    processRegister: function (req, res) {
+        let result = validationResult(req);
+        if (!result.isEmpty()) {
+            return res.render('register', {
+                errors: result.errors,
+                data: req.body
+            })
+        }
+
+        let passwordEncriptada = bcrypt.hashSync(req.body.password, 10);
+
+        const newUser = {
+            email: req.body.email,
+            password: passwordEncriptada
+        };
+
+        let users = fs.readFileSync('src/data/users.json', { encoding: 'utf-8' })
+        users = JSON.parse(users);
+        users.push(newUser);
+        users = JSON.stringify(users);
+        fs.writeFileSync('src/data/users.json', users);
+
+        return res.redirect(301, '/users/login');
+    },
+    login: function (req, res) {
+        return res.render('login');
+    },
+    processLogin: function (req, res) {
+        let result = validationResult(req);
+        if (result.isEmpty()) {
+            let usersJSON = fs.readFileSync('src/data/users.json', { encoding: 'utf-8' })
+            let users;
+            if (usersJSON == " ") {
+                users = [];
+            } else {
+                users = JSON.parse(usersJSON);
+            }
+            let usuarioALoguearse;
+            for (let i = 0; i < users.length; i++) {
+                if (users[i].email == req.body.email) {
+                    if (bcrypt.compareSync(req.body.password, users[i].password)) {
+                        usuarioALoguearse = users[i];
+                        break;
+                    }
+                }
+            }
+            if (usuarioALoguearse == undefined) {
+                return res.render('login', {
+                    errors: [{ msg: "►Usuario y/o contraseña incorrecto/s" }]
+                });
+            }
+            req.session.usuarioLogueado = usuarioALoguearse;
+            res.redirect(301, '/users/welcome');
+        } else {
+            return res.render('login', {
+                errors: result.errors,
+                data: req.body
+            })
+        }
+    },
+    welcome: function (req, res) {
+        res.end(`Bienvenido: ` + req.session.usuarioLogueado.email);
+    }
+}
+
+module.exports = usersController;
--- proyecto-dh/src/routes/users.js
+++ proyecto-dh/src/routes/users.js
@@ -4,62 +4,27 @@ const { check, validationResult, body } = require('express-validator');
 const fs = require('fs');
 const bcrypt = require('bcrypt');
 
+
 let userValidation = [
   check('email').isEmail().withMessage('► El campo debe ser un email válido'),
   check('password').isLength({ min: 4, max: 20 }).withMessage('► El password debe tener entre 4 y 20 caracteres'),
 ]
 
+/* Users Controller */
+const usersController = require('../controllers/usersController');
+
 /* GET users listing. */
-router.get('/login', function (req, res, next) {
-  res.render('login');
-});
 
-router.post('/login', userValidation, function (req, res) {
-  let result = validationResult(req);
-  console.log(result);
-  if (!result.isEmpty()) {
-    return res.render('login', {
-      errors: result.errors,
-      data: req.body
-    })
-  }
-  res.redirect(301, '/users/welcome');
-});
+/*login*/
+router.get('/login', usersController.login);
+router.post('/login', userValidation, usersController.processLogin);
 
+/* register */
+router.get('/register', usersController.register);
+router.post('/register', userValidation, usersController.processRegister);
 
-router.get('/register', function (req, res) {
-  res.render('register');
-});
-
-router.post('/register', userValidation, function (req, res) {
-  let result = validationResult(req);
-
-  if (!result.isEmpty()) {
-    return res.render('register', {
-      errors: result.errors,
-      data: req.body
-    })
-  }
-
-  let passwordEncriptada = bcrypt.hashSync(req.body.password,10);
-
-  const newUser = {
-    email: req.body.email,
-    password: passwordEncriptada
-  };
-
-  let users = fs.readFileSync('src/data/users.json', { encoding: 'utf-8' })
-  users = JSON.parse(users);
-  users.push(newUser);
-  users = JSON.stringify(users);
-  fs.writeFileSync('src/data/users.json', users);
-
-  res.redirect(301, '/users/login');
-});
-
-router.get('/welcome', function (req, res) {
-  res.end(`Bienvenido: `);
-});
+/* welcome */
+router.get('/welcome', usersController.welcome);
 
 
 module.exports = router;

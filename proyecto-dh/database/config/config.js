c4e3c039607c099e6ab235793cbf462eabadbf2b
MatyAlfonso <matt1998boca@gmail.com>
2020-03-19 15:25:47 -0300

    arreglos de la base de datos

--- proyecto-dh/database/config/config.js
+++ proyecto-dh/database/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": "root",
+    "password": null,
     "database": "structure",
     "host": "localhost",
     "dialect": "mysql",
